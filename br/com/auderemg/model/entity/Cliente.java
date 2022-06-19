f5b66332bace3da9632851ed3a4b24cbcc9b09cb
Messias Martins <messiasmartins@outlook.com>
2022-06-19 01:37:44 -0300

    Implementando metodo Get Clietes

--- br/com/auderemg/model/entity/Cliente.java
+++ br/com/auderemg/model/entity/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.auderemg.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,11 +19,12 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(nullable = false, length = 150)
+   @Column(nullable = false, length = 150)
     private String nome;
     @Column(nullable = false, length = 11)
     private String cpf;
     @Column(name = "data_cadastro")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
     @PrePersist
